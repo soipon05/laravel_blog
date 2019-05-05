4e5075dedf0aa40d74ea391f338d4faa76d59fb6
JUN <soipon0@gmail.com>
2019-05-06 01:51:18 +0900

    データの抽出をコントローラで

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostsController extends Controller
 {
     //
     public function index()
     {
+        // $posts = \App\Post::all();
+        // $posts = Post::all();
+        // $posts = Post::orderBy('created_at', 'desc')->get();
+        $posts = Post::latest()->get();
+        dd($posts->toArray());
         return view('posts.index');
     }
 }
